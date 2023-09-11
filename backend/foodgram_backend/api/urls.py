e1de8be81f68106a298ae4b87117b56c348c131a
Екатерина Густова <antinpanic@gmail.com>
2023-09-11 18:28:41 +0300

    change djoser serializers, add subscription дшlist

--- backend/foodgram_backend/api/urls.py
+++ backend/foodgram_backend/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     TagViewSet,
     IngredientViewSet,
-    RecipeViewSet
-)
+    RecipeViewSet,
+    SubscribeViewSet)
 
 app_name = 'api'
 
@@ -12,6 +12,9 @@ router = DefaultRouter()
 router.register(r'tags', TagViewSet)
 router.register(r'ingredients', IngredientViewSet)
 router.register(r'recipes', RecipeViewSet)
+router.register(r'users/subscribtions', SubscribeViewSet, basename='subscribtions')
+# router.register(r'users/(?P<user_id>\d+)/subscribe', SubscribeViewSet,
+#                 basename='subscribe')
 
 urlpatterns = [
     path('', include(router.urls)),
